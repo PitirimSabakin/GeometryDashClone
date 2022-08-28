e804e388830673d30efaee2711ea598951584de9
P1tirim <andrey.orishev@mail.ru>
2022-08-28 16:08:55 +0300

    v0.0161
    A pause menu has been created in which you can continue the game, start the level again and exit the menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject floor;
     [SerializeField] private float speedMap;
     [SerializeField] private TMP_Text textAttempt;
+    [SerializeField] private GameObject panel;
+
+    private Button pauseBtn;
+    private Button exitBtn;
+    private Button restartBtn;
+    private Button continueBtn;
+    
+
+    private Player playerScript;
 
     private Vector3 startPositionX;
     private int attempt = 1;
@@ -16,7 +27,13 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
         startPositionX = floor.transform.position;
+        attempt = 1;
+
+        ButtonInitialization();
+        ContinueGame();
     }
 
     // Update is called once per frame
@@ -37,5 +54,41 @@ public class GameManager : MonoBehaviour
         floor.transform.position = startPositionX;
         attempt++;
         textAttempt.text ="Attempt " + attempt.ToString();
+    }
+
+    private void ButtonInitialization()
+    {
+        pauseBtn = GameObject.Find("ButtonPause").GetComponent<Button>();
+        exitBtn = panel.transform.GetChild(0).GetComponent<Button>();
+        restartBtn = panel.transform.GetChild(1).GetComponent<Button>();
+        continueBtn = panel.transform.GetChild(2).GetComponent<Button>();
+
+        pauseBtn.onClick.AddListener(Pause);
+        exitBtn.onClick.AddListener(ExitInMenu);
+        restartBtn.onClick.AddListener(Restart);
+        continueBtn.onClick.AddListener(ContinueGame);
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = 0;
+        panel.SetActive(true);
+    }
+
+    private void ExitInMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    private void Restart()
+    {
+        playerScript.DeathHandler();
+        ContinueGame();
+    }
+
+    private void ContinueGame()
+    {
+        Time.timeScale = 1;
+        panel.SetActive(false);
     }
 }
